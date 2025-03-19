08f4e93a5fe4744ae099fbeed6fa05562eeb4a0a
KennethAlAr <kennethalonsoarce@gmail.com>
2025-03-19 20:54:24 +0100

    Commit inicial

---
+++ Reto_java_1/src/Reto_java_1.java
@@ -0,0 +1,35 @@
+public class Reto_java_1 {
+
+    public static void main(String[] args) {
+        /*
+        1. Crea un programa en Java que calcule la calificación de un estudiante a partir de un número entero. Solicita un número (x) al usuario y responde de la siguiente manera:
+
+        10 - Matrícula de honor
+        9 - Sobresaliente
+        7.8 - Notable
+        6 - Bien
+        5 - Aprobado
+        0.4 - Suspenso
+        Utiliza la instrucción switch con los casos break y default en Java.
+
+
+        2. Cree un programa en Java que solicite al usuario una letra (x) y compruebe si es una vocal o cualquier otro símbolo. El programa debe mostrar por pantalla "Vocal" si la entrada es una vocal (a, e, i, o, u) y "Otro símbolo" si es cualquier otro carácter.
+
+        3. Escribe un programa en Java que solicite al usuario ingresar tres números (x, y, z) y determine cuál de ellos es el número más grande. Luego, muestra ese número en la pantalla.
+
+
+        4. Escribe un programa en Java que actúe como una calculadora básica. El programa solicitará al usuario ingresar dos números y una operación matemática (+, -, x, /). Luego, calculará la operación seleccionada y mostrará el resultado en la pantalla.
+
+        Si el símbolo de la operación es diferente a los operadores permitidos (+, -, x, /), el programa indicará "Carácter no reconocido".
+
+        5. Escribe un programa en Java que pregunte al usuario por un número x y muestre el resultado de su tabla de multiplicar del 1 al 10.
+
+        6. Escribe un programa en Java que muestre los números impares desde x hasta y en orden descendente.
+
+        7. Crea un programa en Java que simule una caja registradora de supermercado para devolver el cambio de una compra. Solicita la cantidad total de dinero que tienes en la caja registradora (x) y el valor de la compra (y) como un número entero.
+
+        Debes utilizar billetes de 100, 50, 20, 10, 5, 2 o 1 y devolver el cambio utilizando primero los billetes de mayor valor.
+        */
+    }
+
+}
